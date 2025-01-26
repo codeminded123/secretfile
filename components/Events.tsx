7b9bdb17f248d86ec8d62eae5c18740059da5c52
codeminded123 <kanikagotharsh@gmail.com>
2025-01-26 21:59:25 +0530

    Update Events.tsx

--- components/Events.tsx
+++ components/Events.tsx
@@ -2,11 +2,20 @@
 
 import Image from 'next/image'
 
+const eventImage = [
+  '/events/image1.jpeg',
+  '/events/image2.jpg',
+  '/events/image3.jpeg',
+  '/events/image4.jpg',
+  '/events/image5.jpeg',
+  '/events/image6.jpeg',
+]
+
 export default function Events() {
   return (
     <div
       id='events'
-      className='text-center bg-white space-y-9 flex flex-col p-16 mx-auto text-black'
+      className='text-center bg-white space-y-9 flex flex-col p-8 md:p-16 mx-auto text-black'
     >
       <div className='mx-auto'>
         <Image
@@ -20,6 +29,18 @@ export default function Events() {
       <h2 className='text-6xl italic font-semibold font-tangerine'>
         Wedding Events
       </h2>
+      <div className='mx-auto space-y-3'>
+        {eventImage.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            alt={`Event ${index + 1}`}
+            width={1000} // Adjust width as needed
+            height={500} // Adjust height as needed
+            className='w-full h-full border-2 border-black'
+          />
+        ))}
+      </div>
     </div>
   )
 }
